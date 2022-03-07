d70db1af8e0c7cfb4956a4318a69f5d350a1d4c9
veyron104 <70129766+veyron104@users.noreply.github.com>
2022-03-08 01:31:52 +0800

    Les2

---
+++ Assets/Scripts/IInteractive.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains
+{
+    interface IInteractive
+    {
+        void Vizualization();
+        void Interaction();
+    }
+}
