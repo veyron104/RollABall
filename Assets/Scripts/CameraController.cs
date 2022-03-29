c2f01bd17e8a3e70663eaa446afbce25a3189b7c
veyron104 <70129766+veyron104@users.noreply.github.com>
2022-03-30 01:04:09 +0800

    Less5

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,15 @@
 
 namespace Geekbrains
 {
+<<<<<<< Updated upstream
     public sealed class CameraController : MonoBehaviour
+=======
+    public PlayerBall Player;
+    Vector3 _shakerOffset;
+    public Vector3 _offset;
+
+    private void Start ()
+>>>>>>> Stashed changes
     {
         public PlayerBall Player;
         public Vector3 _offset;
